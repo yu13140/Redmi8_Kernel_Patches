93362728a8792f290fd905d37bf1418ee0ed6124
yu13140 <whmyc801@gmail.com>
2025-04-26 17:33:17 +0800

    no sh

--- yuzkernel.sh
+++
@@ -1,7 +0,0 @@
-echo "======start repo sync======"
-repo sync -c -j32 --no-clone-bundle
-while [ $? = 1 ]; do
-    echo "======sync failed, re-sync again======"
-    sleep 3
-    repo sync -c -j32 --no-clone-bundle
-done
