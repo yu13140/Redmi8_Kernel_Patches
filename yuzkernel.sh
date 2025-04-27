586048bc331da3c265c2466b3ddfdf5f26bad92b
yu13140 <whmyc801@gmail.com>
2025-04-27 11:42:39 +0800

    debug

--- yuzkernel.sh
+++ yuzkernel.sh
@@ -1,9 +1,16 @@
 echo "======start repo sync======"
+failure=0
 repo sync -c -j32 --no-clone-bundle
-while [ $? = 1 ]; do
+while [ $? -ne 0 ]; do
     echo "======get remaining space======"
     df -hT
     echo "======sync failed, re-sync again======"
+    failure=$(($failure+1))
     sleep 3
     repo sync -c -j32 --no-clone-bundle
 done
+
+if [ $failure = 10 ]; then
+    echo "======Too many attempts======"
+    exit 1
+fi
